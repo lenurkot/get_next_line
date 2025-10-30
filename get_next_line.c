8b96b6b7951fc81aa2401bd267f84f0fc94f3263
Elena Kotova <ekotova@f0r2s8.codam.nl>
2025-10-30 14:07:08 +0100

    understand the algorithm. have to figure out the realloc

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: ekotova <ekotova@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 16:38:57 by ekotova           #+#    #+#             */
-/*   Updated: 2025/10/27 13:41:33 by ekotova          ###   ########.fr       */
+/*   Updated: 2025/10/30 14:05:50 by ekotova          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /* The read() function shall attempt
@@ -29,50 +30,65 @@ occurred */
 
 char	*get_next_line(int fd)
 {
-	char			buf[BUFSIZ];
-	static size_t	line_to_read;
+	static char		buf[BUFFER_SIZE];
+	char			*str;
+	static size_t	read_line;
 	int				i;
 
-	// char			*buf;
-	// static size_t	line_to_read;
-	// if (fd == -1)
-	// 	return (NULL);
-	// buf = malloc(BUFSIZ + 1);
-	// if (line_to_read > 0)
-	// {
-	// 	line_to_read = read(fd, buf, BUFSIZ - 1);
-	// 	buf[line_to_read] = '\n';
-	// }
-	// printf("buf file - %s\n", buf);
-	// return (buf);
+	i = 0;
 	if (fd == -1)
 		return (NULL);
-	line_to_read = read(fd, buf, BUFSIZ - 1);
+	str = malloc(BUFFER_SIZE + 1);
+	if (str == NULL)
+		return (NULL);
+	read_line = read(fd + i, buf, BUFFER_SIZE - 1);
+	buf[read_line] = '\n';
 	while (buf[i] != '\n')
 	{
+		str[i] = buf[i];
 		i++;
 	}
-	buf[i] = '\0';
-	// printf("I've read - %s\n", buf);
-	// while (line_to_read > 0)
+	if (buf[i] == '\n')
+		str[i] = '\n';
+	// printf("read line - %zu, line - %s\n", read_line, str);
+	// printf("i m gonna crush\n");
+	// read_line = read(fd, buf, BUFFER_SIZE - 1);
+	// printf("read line - %zu\n", read_line);
+	// printf("you don't see me\n");
+	// buf[read_line] = '\n';
+	// while (buf[i] != '\n' || buf[i])
 	// {
-	// 	buf[line_to_read] = '\n';
-	// 	printf("I've read - %s\n", buf);
-	// 	line_to_read = read(fd, buf, BUFSIZ - 1);
+	// 	printf("i was here\n");
+	// 	str[i] = buf[i];
+	// 	i++;
 	// }
-	return (buf);
+	// if (buf[i] != '\n')
+	// 	str[i] = '\n';
+	// printf("read line - %zu, line - %s\n", read_line, str);
+	// while (read_line > 0)
+	// {
+	// 	buf[read_line] = '\n';
+	// 	read_line = read(fd, buf, BUFFER_SIZE - 1);
+	// }
+	return (str);
 }
 
 int	main(int argc, char *argv[])
 {
-	char	*line_to_read;
 	char	*name_file;
+	char	*line_to_read;
 	int		fd;
 
-	name_file = argv[1];
+	name_file = argv[argc - 1];
+	// printf("name file - %s\n, argc = %d\n", argv[argc - 1], argc);
 	fd = open(name_file, O_RDONLY);
 	line_to_read = get_next_line(fd);
+	// line_to_read = get_next_line(fd);
 	// close(fd);
 	printf("stirng from file - %s\n", line_to_read);
+	line_to_read = get_next_line(fd);
+	printf("stirng from file - %s\n", line_to_read);
+	// line_to_read = get_next_line(fd);
+	// printf("stirng from file - %s\n", line_to_read);
 	return (0);
 }
