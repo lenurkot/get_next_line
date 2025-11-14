fbc1f857ffa391f0139daf7f09a4b04c0341b186
Elena Kotova <ekotova@f0r1s2.codam.nl>
2025-11-14 14:04:59 +0100

    handled if BUF_SIZE <= 0

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ekotova <ekotova@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 16:37:07 by ekotova           #+#    #+#             */
-/*   Updated: 2025/11/05 19:03:53 by ekotova          ###   ########.fr       */
+/*   Updated: 2025/11/14 13:39:41 by ekotova          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 50
 # endif
+
+#if BUFFER_SIZE < 1
+    #error "BUFFER_SIZE SHOULD BE AT LEAST 1"
+#endif
 
 char	*get_next_line(int fd);
 
