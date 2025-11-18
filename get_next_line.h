22b62cf13c87d6aa1769da06c2ad09a4a183d70a
Elena Kotova <ekotova@f0r1s6.codam.nl>
2025-11-18 15:50:31 +0100

    moved all variables into struct. Added func which moved to util.c

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ekotova <ekotova@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 16:37:07 by ekotova           #+#    #+#             */
-/*   Updated: 2025/11/14 17:52:53 by ekotova          ###   ########.fr       */
+/*   Updated: 2025/11/18 15:36:39 by ekotova          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,28 @@
 #  define BUFFER_SIZE 50
 # endif
 
-#if BUFFER_SIZE < 1
-    #error "BUFFER_SIZE SHOULD BE AT LEAST 1"
-#endif
+# if BUFFER_SIZE < 1
+#  error "BUFFER_SIZE SHOULD BE AT LEAST 1"
+# endif
+# include <stdlib.h>
 
-char	*get_next_line(int fd);
+typedef struct s_states
+{
+	char	buf[BUFFER_SIZE];
+	int		buf_pos;
+	int		buf_len;
+	int		is_line;
+	int		current_fd;
+	char	*line;
+	int		line_len;
+	int		line_capacity;
+}			t_buf_stat;
+
+char		*get_next_line(int fd);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+char		*ft_realloc(char *str, size_t to_copy, size_t new_len);
+int			is_new_line(char *buf, int buf_len, int *is_line);
+void		reset_state(t_buf_stat *st);
+int			ensure_line_capacity(t_buf_stat *st, int buf_len);
 
 #endif
